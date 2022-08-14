f8be3cdf9317561c655ba6066b92da5080203b59
Manish Kumar <manish.k@binmile.com>
2022-08-14 13:56:06 +0530

    Changes done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,21 +6,21 @@ $( ".img-wrapper" ).hover(
       $(this).find(".img-overlay").animate({opacity: 0}, 600);
     }
   );
-  
+
   // Lightbox
   var $overlay = $('<div id="overlay"></div>');
   var $image = $("<img>");
   var $prevButton = $('<div id="prevButton"><i class="fa fa-chevron-left"></i></div>');
   var $nextButton = $('<div id="nextButton"><i class="fa fa-chevron-right"></i></div>');
   var $exitButton = $('<div id="exitButton"><i class="fa fa-times"></i></div>');
-  
+
   // Add overlay
   $overlay.append($image).prepend($prevButton).append($nextButton).append($exitButton);
   $("#gallery").append($overlay);
-  
+
   // Hide overlay on default
   $overlay.hide();
-  
+
   // When an image is clicked
   $(".img-overlay").click(function(event) {
     // Prevents default behavior
@@ -32,13 +32,13 @@ $( ".img-wrapper" ).hover(
     // Fade in the overlay
     $overlay.fadeIn("slow");
   });
-  
+
   // When the overlay is clicked
   $overlay.click(function() {
     // Fade out the overlay
     $(this).fadeOut("slow");
   });
-  
+
   // When next button is clicked
   $nextButton.click(function(event) {
     // Hide the current image
@@ -52,7 +52,7 @@ $( ".img-wrapper" ).hover(
     // All of the images in the gallery
     var $images = $("#image-gallery img");
     // If there is a next image
-    if ($nextImg.length > 0) { 
+    if ($nextImg.length > 0) {
       // Fade in the next image
       $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
     } else {
@@ -62,7 +62,6 @@ $( ".img-wrapper" ).hover(
     // Prevents overlay from being hidden
     event.stopPropagation();
   });
-  
   // When previous button is clicked
   $prevButton.click(function(event) {
     // Hide the current image
@@ -78,7 +77,6 @@ $( ".img-wrapper" ).hover(
     // Prevents overlay from being hidden
     event.stopPropagation();
   });
-  
   // When the exit button is clicked
   $exitButton.click(function() {
     // Fade out the overlay
@@ -87,5 +85,102 @@ $( ".img-wrapper" ).hover(
 
 
 
- 
-  
+  $(document).ready(function() {
+
+    var form = $('#form'),
+        txt_name = $('#txt_name'),
+        txt_phone = $('#txt_phone'),
+        txt_email = $('#txt_email'),
+        txt_pincode = $('#txt_pincode'),
+        txt_property_size = $('#txt_property_size'),
+        txt_location = $('#txt_location'),
+        txt_property_type = $('#txt_property_type'),
+        txt_area = $('#txt_area'),
+        info = $('#info'),
+        loader = $('#loader'),
+        submit = $("#submit");
+
+    form.on('input', '#email, #subject, #message', function() {
+      $(this).css('border-color', '');
+      info.html('').slideUp();
+    });
+
+    submit.on('click', function(e) {
+      info.html('All fields are mandatory...').css('color', 'red').slideDown();
+      e.preventDefault();
+      if(validate()) {
+        info.html('');
+        $.ajax({
+          type: "POST",
+          url: "mailer.php",
+          data: form.serialize(),
+          dataType: "json"
+        }).done(function(data) {
+          if(data.success) {
+            email.val('');
+            subject.val('');
+            message.val('');
+            info.html('You have Message sent!').css('color', 'green').slideDown();
+          } else {
+            info.html('Could not send mail! Sorry!').css('color', 'red').slideDown();
+          }
+        });
+      }
+    });
+
+    function validate() {
+      var valid = true;
+      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+      if($.trim(txt_name.val()) === "") {
+        txt_name.css('border-color', 'red');
+        valid = false;
+      } else {
+        txt_name.css('border-color', '');
+      }
+      if($.trim(txt_phone.val()) === "") {
+        txt_phone.css('border-color', 'red');
+        valid = false;
+      } else {
+        txt_phone.css('border-color', '');
+      }
+      if(!regex.test(txt_email.val())) {
+        txt_email.css('border-color', 'red');
+        valid = false;
+      }else {
+        txt_email.css('border-color', '');
+      }
+      if($.trim(txt_location.val()) === "") {
+        txt_location.css('border-color', 'red');
+        valid = false;
+      } else {
+        txt_location.css('border-color', '');
+      }
+      if($.trim(txt_pincode.val()) === "") {
+        txt_pincode.css('border-color', 'red');
+        valid = false;
+      } else {
+        txt_pincode.css('border-color', '');
+      }
+      if($.trim(txt_property_type.val()) === "") {
+        txt_property_type.css('border-color', 'red');
+        valid = false;
+      } else {
+        txt_property_type.css('border-color', '');
+      }
+      if($.trim(txt_property_size.val()) === "") {
+        txt_property_size.css('border-color', 'red');
+        valid = false;
+      } else {
+        txt_property_size.css('border-color', '');
+      }
+      if($.trim(txt_area.val()) === "") {
+        txt_area.css('border-color', 'red');
+        valid = false;
+      } else {
+        txt_area.css('border-color', '');
+      }
+      return valid;
+    }
+
+  });
